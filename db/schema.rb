28953efd661d3decf69be2302ad075f9ecfdf66b
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-02 12:43:48 +0900

    Remove Unnecessary migration and model file
    # What
    Message投稿画面のビュー作成では、
    不要なマイグレーションファイルとモデルファイルの削除
    
    # Why
    現段階では必要のないファイル。
    ※ご指摘いただいた内容は、usersテーブルやgroupsテーブル作成後に作る中間テーブルにて対応いたします。

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227011005) do
-
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "image"
-    t.text     "content",    limit: 65535
-    t.integer  "user",                     null: false
-    t.integer  "group",                    null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
