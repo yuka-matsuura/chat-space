04308ade7704f966004d746cbda066f78fffabab
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-03 20:45:56 +0900

    Create group creation function
    # What
    グループ作成機能実装
    
    # Why
    グループ作成機能は必須のため

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,17 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
-
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,7 +23,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, [])
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 
 end
