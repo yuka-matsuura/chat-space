4b228462023c5ce414059c99c0689cbde651c3e4
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-04 17:42:07 +0900

    Add message route
    # What
    ルーティングの更新
    
    # Why
    メッセージの保存を行うcreateは必要だから

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
   devise_for :users
 end
