7be07bcf8731fc1e18ef18e946b3cc80a0580306
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-06 18:05:10 +0900

    Make devise available to rspec
    # What
    deviseをrspecで使用できるように設定
    
    # Why
    Controllerのテストに必要だから

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
