3aa0f843cd8928982ed1cc6bac081bfd207636f3
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-10 14:55:49 +0900

    Add incremental Search after
    # What
    インクリメンタルサーチ後の処理追加
    
    # Why
    仕様通りの動作にするため

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,31 @@
 $(function(){
 
-  var search_list = $("#user-search-result");
+  var searchList = $("#user-search-result");
 
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
-    search_list.append(html);
+    searchList.append(html);
   }
 
   function appendNoUser(user) {
     var html = `<div class="chat-group-user clearfix">${user}</div>`
-    search_list.append(html);
+    searchList.append(html);
+  }
+
+  function addUser(id,name){
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">
+                  <input name="group[user_ids][]" type="hidden" value="${id}"></input>
+                  <p class="chat-group-user__name">${name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+                </div>`
+    $('#chat-group-users').append(html);
   }
 
   // インクリメンタルサーチ
-  $(".chat-group-form__input").on("keyup", function() {
+  $('#user-search-field').on('keyup', function() {
     var input = $(this).val();
 
     $.ajax({
@@ -27,7 +36,7 @@ $(function(){
     })
     .done(function(users){
       // 成功した時の処理
-      search_list.empty();
+      searchList.empty();
       if( users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
@@ -42,4 +51,17 @@ $(function(){
       alert('error');
     })
   });
+
+  // インクリメンタルサーチ後の処理
+  $(document).on('click', '.chat-group-user__btn--add', function(){
+    var id = $(this).attr('data-user-id');
+    var name = $(this).attr('data-user-name');
+    addUser(id,name);
+    $(this).parent().remove();
+  });
+
+  $(document).on('click', '.js-remove-btn', function(){
+    $(this).parent().remove();
+  });
+
 });
