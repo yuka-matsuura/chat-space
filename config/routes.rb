d90ed16c8958d147b2b457219c88f7d25b79d309
yuka-matsuura <appleidmtk@outlook.jp>
2018-03-10 16:53:23 +0900

    Change routing
    # What
    アクションの順番変更
    
    # Why
    可読性向上のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "groups#index"
-  resources :users, only: [:edit, :update, :index]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
